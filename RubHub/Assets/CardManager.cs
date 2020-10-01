f0026c0d04e1d7f4f3a2752edc84946e99093cd9
Stephen Greenland <skipsy10@gmail.com>
2020-10-01 14:30:33 +1000

    Added Unity Project
    if bryce ever gets on GitHub

---
+++ RubHub/Assets/CardManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    private Array[,] cards;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cards = new Array[4,5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
